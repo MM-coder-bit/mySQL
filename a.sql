3946b58b8f669244614ca8dcbe7e9a61e9f2283b
Mateus <marques18@outlook.com>
2023-02-17 11:51:09 -0300

    criando uma tabela pelo comando 'CREATE TABLE'

--- a.sql
+++ a.sql
@@ -1,4 +1,4 @@
 # C:\ProgramData\MySQL\MySQL Server 8.0 ---> Local fisico onde se encontra o banco de dados
 create database SUCOS; # maneira de criar uma tabela (banco de dados)
 
-drop database SUCOS; # Maneira de apagar banco de dados
+drop database SUCOS2; # Maneira de apagar banco de dados
