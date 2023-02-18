50e658bb7f11f653778245c4dcb25af5d2f5e11c
Mateus <marques18@outlook.com>
2023-02-18 19:15:46 -0300

    adicionado chave primária

---
+++ tb_chave_primaria.sql
@@ -0,0 +1,16 @@
+USE sucos;
+
+# Comando para definir o 'PRODUTO" como chave primária
+ALTER TABLE tbproduto ADD PRIMARY KEY (PRODUTO);
+
+SELECT * FROM tbproduto;
+
+# Validando a config de chave primária
+# se rodar mais que duas vezes vai ocorrer erro
+INSERT INTO tbproduto (
+PRODUTO,  NOME, EMBALAGEM, TAMANHO, SABOR,
+PRECO_LISTA) VALUES
+('1078680', 'Frescor do Verão - 470 ml - Manga', 'Lata', '470 ml','Manga',5.18);
+
+UPDATE tbproduto SET EMBALAGEM = 'Garrafa'
+WHERE PRODUTO = '1078680';
