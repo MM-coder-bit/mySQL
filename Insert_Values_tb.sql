ecc4e9b2ca727a65d7a71d3e22e4b9639ee25a0c
Mateus <marques18@outlook.com>
2023-02-18 15:42:57 -0300

    comando para inserir valores no banco de dados 'Insert into' e  'values'

---
+++ Insert_Values_tb.sql
@@ -0,0 +1,8 @@
+USE Sucos;
+
+INSERT INTO tbproduto(
+PRODUTO,NOME,EMBALAGEM,TAMANHO,SABOR,PRECO_LISTA) 
+VALUES(
+'1040107', 'Light - 350 ml - Melancia', 'Lata', '350 ml', 'Melancia', 4.56);
+
+SELECT * FROM tbproduto;
