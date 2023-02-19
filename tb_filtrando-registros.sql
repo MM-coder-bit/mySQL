a76f232565cd1a0e7ebf88ccdd092bbafa9e4310
Mateus <marques18@outlook.com>
2023-02-18 21:46:30 -0300

    maneiras de realizar filtros com sinais de >, <, <> e 'between'

--- tb_filtrando-registros.sql
+++ tb_filtrando-registros.sql
@@ -9,3 +9,33 @@ select * from tbproduto WHERE SABOR = "Limão";
 UPDATE tbproduto SET SABOR = 'Cítricos' WHERE SABOR = 'Limão';
 # apresenta as novas alterações
 select * from tbproduto WHERE SABOR = "Cítricos";
+
+# ---------------------------------------------------- #
+# ---- Filtrando usando maior, menor e diferente ----- #
+SELECT * FROM tbcliente;
+
+SELECT * FROM tbcliente WHERE IDADE = 22;
+
+SELECT * FROM tbcliente WHERE IDADE > 22;
+
+SELECT * FROM tbcliente WHERE IDADE < 22;
+
+SELECT * FROM tbcliente WHERE IDADE <= 22;
+
+SELECT * FROM tbcliente WHERE IDADE <> 22;
+
+# Seleciona por ordem alfabética
+SELECT * FROM tbcliente WHERE NOME >= 'Fernando Cavalcante';
+# apresenta todos menos Fernando
+SELECT * FROM tbcliente WHERE NOME <> 'Fernando Cavalcante';
+
+SELECT * FROM tbproduto;
+
+SELECT * FROM tbproduto WHERE PRECO_LISTA > 16.008;
+
+SELECT * FROM tbproduto WHERE PRECO_LISTA < 16.008;
+
+SELECT * FROM tbproduto WHERE PRECO_LISTA <> 16.008;
+
+# Apresenta uma faixa desejada entre 16.007 a 16.009
+SELECT * FROM tbproduto WHERE PRECO_LISTA BETWEEN 16.007 AND 16.009;
